d7ce1e586a00bce07405715c5d086011436c0211
sebseb7 <github@seb.exse.net>
2012-01-26 02:57:48 +0100

    pre alpha

---
+++ firmware/pentacode/main.c
@@ -0,0 +1,187 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+
+#include "main.h"
+#include "pins.h"
+
+
+
+
+
+//these variables are used by the timer intr
+uint8_t row_step = 0;
+
+uint8_t rowbyte_portc[12] = {0,0,0,0,0 ,0 ,1,2,4,8,16,32};
+uint8_t rowbyte_portb[12] = {1,2,4,8,16,32,0,0,0,0,0 ,0 };
+
+uint8_t colbyte_portb[128]={
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							};
+
+uint8_t colbyte_portd[128]={
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+							};
+
+
+uint8_t frame_buffer[32];
+
+
+ISR (TIMER1_OVF_vect)
+{
+	row_step++;
+	if(row_step==13)
+	{
+		row_step = 0;
+	}
+
+	PORTD = 0;
+
+	PORTC = rowbyte_portc[row_step];
+	PORTB = rowbyte_portc[row_step];
+
+	PORTD = row_step;
+
+}
+
+
+int main(void)
+{
+	ROW0_DDR |= (1<<ROW0_PIN);
+	ROW1_DDR |= (1<<ROW1_PIN);
+	ROW2_DDR |= (1<<ROW2_PIN);
+	ROW3_DDR |= (1<<ROW3_PIN);
+	ROW4_DDR |= (1<<ROW4_PIN);
+	ROW5_DDR |= (1<<ROW5_PIN);
+	ROW6_DDR |= (1<<ROW6_PIN);
+	ROW7_DDR |= (1<<ROW7_PIN);
+
+	COL0_DDR |= (1<<COL0_PIN);
+	COL1_DDR |= (1<<COL1_PIN);
+	COL2_DDR |= (1<<COL2_PIN);
+	COL3_DDR |= (1<<COL3_PIN);
+	COL4_DDR |= (1<<COL4_PIN);
+	COL5_DDR |= (1<<COL5_PIN);
+	COL6_DDR |= (1<<COL6_PIN);
+	COL7_DDR |= (1<<COL7_PIN);
+	COL8_DDR |= (1<<COL8_PIN);
+	COL9_DDR |= (1<<COL9_PIN);
+	COL10_DDR |= (1<<COL10_PIN);
+	COL11_DDR |= (1<<COL11_PIN);
+
+
+	ROW0_OFF;
+	ROW1_OFF;
+	ROW2_OFF;
+	ROW3_OFF;
+	ROW4_OFF;
+	ROW5_OFF;
+	ROW6_OFF;
+	ROW7_OFF;
+
+	// only one col at a time !!
+	COL0_OFF;
+	COL1_OFF;
+	COL2_OFF;
+	COL3_OFF;
+	COL4_OFF;
+	COL5_OFF;
+	COL6_OFF;
+	COL7_OFF;
+	COL8_OFF;
+	COL9_OFF;
+	COL10_OFF;
+	COL11_OFF;
+		
+
+	TCCR1A |= (1<<WGM10)|(1<<WGM11);
+	TCCR1B |= (1<<WGM12)|(1<<WGM13)|(1<<CS10);
+	TIMSK1 |= (1<<TOIE1);
+	OCR1A = 0x50;
+	
+	sei();
+	
+	setLedAll(0);
+
+
+	setLedXY(5,5,1);
+}
+
+
+void setLedXY(uint8_t x,uint8_t y, uint8_t brightness)
+{
+	if(y > 1)
+	{
+		for(uint8_t i = 0;i < 15;i++)
+		{
+			if(brightness > i)
+			{
+				colbyte_portb[x*16+i]|=(1<<(y-2));
+			}
+			else
+			{
+				colbyte_portb[x*16+i]&=~(1<<(y-2));
+			}
+		}
+	}
+	else
+	{
+		for(uint8_t i = 0;i < 15;i++)
+		{
+			if(brightness > i)
+			{
+				colbyte_portd[x*16+i]|=(1<<(y+6));
+			}
+			else
+			{
+				colbyte_portd[x*16+i]&=~(1<<(y+6));
+			}
+		}
+	}
+}
+
+void setLedAll(uint8_t brightness)
+{
+	for(uint8_t x = 0;x<8;x++)
+	{
+		for(uint8_t i = 0;i < 15;i++)
+		{
+			if(brightness > i)
+			{
+				colbyte_portb[x*16+i] = 63;
+			}
+			else
+			{
+				colbyte_portb[x*16+i] = 0;
+			}
+		}
+		for(uint8_t i = 0;i < 15;i++)
+		{
+			if(brightness > i)
+			{
+				colbyte_portd[x*16+i]=192;
+			}
+			else
+			{
+				colbyte_portd[x*16+i]=0;
+			}
+		}
+	}
+}
+
---
+++ firmware/pentacode/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+void setLedXY(uint8_t,uint8_t,uint8_t); // x , y , brightness
+void setLedAll(uint8_t); // brightness
+
+#endif
